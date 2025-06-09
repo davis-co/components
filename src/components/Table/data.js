e835beaf26daf5c4ccba6921ddcd457ae3136cb3
maryan100166 <cheginidavis@gmail.com>
2025-06-09 14:40:31 +0330

    fix table pagination numbers

--- src/components/Table/data.js
+++ src/components/Table/data.js
@@ -1,8 +1,8 @@
 export const tableSizeList = [
-    { label: "نمایش به صورت : 5", value: 5 },
     { label: "نمایش به صورت : 10", value: 10 },
     { label: "نمایش به صورت : 20", value: 20 },
-    { label: "نمایش به صورت : 30", value: 30 },
+    { label: "نمایش به صورت : 50", value: 50 },
+    { label: "نمایش به صورت : 100", value: 100 },
 ]
 
 export const excelOptions = [
